ba45f1b52d9c75de00170a0231754b682242ef1e
Vadim Storozhuk <vadim.storo@gmail.com>
2019-05-15 00:08:25 -0400

    End of lecture 09

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
@@ -49,15 +50,6 @@ module.exports = {
                 test: /\.html$/,
                 use: [
                     {
-                        loader: "file-loader",
-                        options: {
-                            name: "[name].html"
-                        }
-                    },
-                    {
-                        loader: "extract-loader"
-                    },
-                    {
                         loader: "html-loader",
                         options: {
                             attrs: ["img:src"]
@@ -79,6 +71,9 @@ module.exports = {
         ]
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new HTMLWebpackPlugin({
+            template: "./src/index.html"
+        })
     ]
 }
--- src/server/main.js
+++ src/server/main.js
@@ -1,2 +1,2 @@
-require("babel-register");
+require("babel-register"); // allows ES6 imports in express.js
 require("./express");
